78535ee391eb776a63296851f5fa0d9d2fd00fe6
KR1LL1N-16 <75556269+KR1LL1N-16@users.noreply.github.com>
2021-01-27 11:52:54 +0700

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,85 +1,85 @@
 #coded by KR1LL1N
 # -- coding: UTF-8 --
-import os,sys,time
+'import os,sys,time'
 
-def main():
-    time.sleep(1)
-    os.system ('clear')
-    print '[96m'
-    os.system ('figlet TOOLS')
-    print'[92m#######################################################################'
-    print'[97m tools by : KR1LL1N '
-    print'[97m Team   : 3XPLOIT.ID '
-    print'[97m criticism & suggestions & bugs : +1 (801) 203-3798 -KR1LL1N '
-    print'[97m Join us:https://chat.whatsapp.com/FixE4PEg9nF9e4PhpreSO1 '
-    print'[97m leader team 3XPLOIT.ID : +1 (205) 672-6711 -MR.L1NU3X- '
-    print'[97m NOTE : We are not responsible for all user actions -Author:KR1LL1N- '
-    print'[92m#######################################################################'
-    print'[92m************** [97mT O O L [92m**************'
-    print'[92m[[97m1[92m] [97mDDOS '
-    print'[92m[[97m2][92m [97mPHISING '
-    print'[92m[[97m3[92m] [97minformation gathering tools '
-    print'[92m[[97m4[92m] [97mWEBDAV'
-    print'[92m[[97m5[92m] [97mSQL '
-    print'[92m[[97m6[92m] [97mPayload/Backdoor Generators '
-    print'[92m[[97m7[92m] [97mWIFI '
-    print'[92m[[97m8[92m] [97mspam and call '
-    print'[92m[[91m0[92m] [91mExit '
-    gans = raw_input ('[97m==>[93m ')
-    if gans in ['1']:
-        time.sleep(1)
-        os.system ('git clone https://github.com/gkbrk/slowloris')
-        print ('[97m$ step 1 [93mcd slowloris')
-        print ('[97m$ step 2 [93mpython3 slowloris.py example.com')
-        exit()
-    if gans in ['2']:
-        time.sleep(1)
-        os.system ('git clone https://github.com/DarkSecDevelopers/HiddenEye-Legacy')
-        print ('[97m$ step 1 [93mcd HiddenEye-Legacy')
-        print ('[97m$ step 2 [93m pip3 install -r requirements.txt ')
-        print ('[97m$ step 3 [93m python3 HiddenEye.py')
-        exit()
-    if gans in ['3']:
-        time.sleep(1)
-        os.system ('git clone https://github.com/Moham3dRiahi/Th3inspector')
-        print ('[97m$ step 1 [93mcd Th3inspector')
-        print ('[97m$ step 2 [93mchmod +x install.sh && ./install.sh')
-        exit()
-    if gans in ['4']:
-        time.sleep(1)
-        os.system ('git clone https://github.com/KR1LL1N-16/KR1LL1N-W3BD4AV')
-        print ('[97m$ step 1 [93mcd KR1LL1N-W3BD4AV')
-        print ('[97m$ step 2 [93mpython W3BD4V')
-        exit()
-    if gans in ['5']:
-        time.sleep(1)
-        os.system('git clone https://github.com/sqlmapproject/sqlmap  ')
-        exit()
-    if gans in ['6']:
-        time.sleep(1)
-        os.system ('git clone https://github.com/M4sc3r4n0/Evil-Droid')
-        print ('[97m$ step 1 [93mcd Evil-Droid')
-        print ('[97m$ step 2 [93mchmod +x evil-droid')
-        print ('[97m$ step 3 [93m./evil-droid')
-        exit()
-    if gans in ['7']:
-        time.sleep(1)
-        os.system('git clone https://github.com/k4m4/kickthemout  ')
-        exit()
-    if gans in ['8']:
-        time.sleep(1)
-        os.system ('git clone https://github.com/TheSpeedX/TBomb')
-        print ('[97m$ step 1 [93mcd TBomb')
-        print ('[97m$ step 2 [93m./TBomb.sh')
-        exit()
-    if gans in ['0']:
-        time.sleep(2)
-        exit()
-            exit()
-    else:
-        time.sleep(1)
-        print '[97m Repeat right you bastard [92m dummed . . .'
-        time.sleep(1)
-        main()
+'def main():
+    'time.sleep(1)'
+    'os.system (clear)'
+    'print ''[96m'
+   'os.system (figlet TOOLS)'
+   'print''[92m#######################################################################'
+   'print''[97mtools by : KR1LL1N '
+   'print''[97mTeam   : 3XPLOIT.ID '
+   'print''[97mcriticism & suggestions & bugs : +1 (801) 203-3798 -KR1LL1N '
+   'print''[97mJoin us:https://chat.whatsapp.com/FixE4PEg9nF9e4PhpreSO1 '
+   'print''[97mleader team 3XPLOIT.ID : +1 (205) 672-6711 -MR.L1NU3X- '
+   'print''[97mNOTE : We are not responsible for all user actions -Author:KR1LL1N- '
+   'print''[92m#######################################################################'
+   'print''[92m************** [97mT O O L [92m**************'
+   'print''[92m[[97m1[92m] [97mDDOS '
+   'print''[92m[[97m2][92m [97mPHISING '
+   'print''[92m[[97m3[92m] [97minformation gathering tools '
+   'print''[92m[[97m4[92m] [97mWEBDAV'
+   'print''[92m[[97m5[92m] [97mSQL '
+   'print''[92m[[97m6[92m] [97mPayload/Backdoor Generators '
+   'print''[92m[[97m7[92m] [97mWIFI '
+   'print''[92m[[97m8[92m] [97mspam and call '
+   'print''[92m[[91m0[92m] [91mExit '
+   'gans = raw_input ([97m==>[93m ')
+   'if gans in ['1']:
+       'time.sleep(1)
+        'os.system' '(git clone https://github.com/gkbrk/slowloris)'
+        'print ([97m$ step 1 [93mcd slowloris)'
+        'print ([97m$ step 2 [93mpython3 slowloris.py example.com)'
+        'exit()'
+    'if gans in ['2']:
+        'time.sleep(1)
+        'os.system (git clone https://github.com/DarkSecDevelopers/HiddenEye-Legacy)'
+        'print ([97m$ step 1 [93mcd HiddenEye-Legacy')
+        'print ([97m$ step 2 [93m pip3 install -r requirements.txt )'
+        'print ([97m$ step 3 [93m python3 HiddenEye.py)'
+        'exit()
+    'if gans in ['3']:
+        'time.sleep(1)
+        'os.system (git clone https://github.com/Moham3dRiahi/Th3inspector)'
+        'print ([97m$ step 1 [93mcd Th3inspector)'
+        'print ([97m$ step 2 [93mchmod +x install.sh && ./install.sh)'
+       'exit()
+    'if gans in ['4']:
+       'time.sleep(1)
+        'os.system (git clone https://github.com/KR1LL1N-16/KR1LL1N-W3BD4AV)'
+        'print ([97m$ step 1 [93mcd KR1LL1N-W3BD4AV)'
+        'print ([97m$ step 2 [93mpython W3BD4V)'
+        'exit()
+    'if gans in ['5']:
+        'time.sleep(1)
+        'os.system(git clone https://github.com/sqlmapproject/sqlmap)'
+        'exit()
+    'if gans in ['6']:
+       'time.sleep(1)
+        'os.system (git clone https://github.com/M4sc3r4n0/Evil-Droid)'
+        'print ([97m$ step 1 [93mcd Evil-Droid)'
+        'print ([97m$ step 2 [93mchmod +x evil-droid)'
+        'print ([97m$ step 3 [93m./evil-droid)'
+        'exit()
+    'f gans in ['7']:
+       'time.sleep(1)
+       'os.system(git clone  https://github.com/k4m4/kickthemout  )'
+       'exit()
+    'f gans in ['8']:
+       'time.sleep(1)
+       'os.system (git clone https://github.com/TheSpeedX/TBomb)'
+       'print ([97m$ step 1 [93mcd TBomb)'
+       'print ([97m$ step 2 [93m./TBomb.sh)'
+       'exit()
+   'if gans in ['0']:
+       'time.sleep(2)
+       'exit()
+           'exit()
+   'else:
+       'time.sleep(1)
+       'print [97m Repeat right you bastard [92m dummed . . .'
+       'time.sleep(1)
+       'main()
 
-main()
+'main()
